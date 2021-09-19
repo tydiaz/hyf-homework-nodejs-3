89b1b16e8bee384421c899801eec46a3a75dfcad
Ty <nodedev21@protonmail.com>
2021-09-19 19:12:44 -0400

    add users/:id endpint

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ app.get('/users', (req, res) => {
   res.send([]);
 });
 
+app.get('/users/:id', (req, res) => {
+  res.json({ user: req.params.id });
+})
+
+app.post('/users', (req, res) => {
+  res.json({ id: 0 });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}....`);
 });
